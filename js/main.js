cd28ed76f9ad0e0fc6d29429ef1494d3a180bfda
Evgeniy-Fedina <75475795+Evgeniy-Fedina@users.noreply.github.com>
2021-04-21 12:07:00 +0300

    resize

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBasic);
-function drawBasic() {
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'X');
-    data.addColumn('number', 'Revenue');
-    data.addColumn('number', '12MMA');
-    data.addRows([
+$( window ).resize(function() {
+    console.log('resize');
+    drawChart();
+});
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Revenue');
+        data.addColumn('number', '12MMA');
+        data.addRows([
     [0, 80, 300],   [0.9, 351, 450], [1.9, 301, 565], [2.9,360, 540], [3.8,200, 390], [4.7,140, 590], [5.7,140, 600], [6.7,250, 660], [7.8,310, 800], [8.7,450, 950], [9.6,515, 900], [11, 700, 1200]
     ]);
     var options = {
@@ -16,7 +19,7 @@ function drawBasic() {
         count: 0
         }
     },
-    width: 1000,
+    width: '100%',
     height: 300,
     legend: {position: 'bottom'},
     is3D: true,
@@ -33,6 +36,6 @@ function drawBasic() {
     },
     colors: ['#63ECDB', '#2F80ED'],
     };
-    var chart = new google.visualization.LineChart(document.getElementById('chart-div'));
-    chart.draw(data, options);
-}
+        var chart = new google.visualization.LineChart(document.getElementById('chart-div'));
+        chart.draw(data, options);
+    }
